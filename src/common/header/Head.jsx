111ac56a33dc147582900a3945439f01b14c5e0a
ridoy113 <almamounhridoy113@gmail.com>
2022-11-16 16:36:26 +0600

    add some new data

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const Head = () => {
   return (
     <>
-        
+      <section className='head'>
+        <div className="container d_flex">
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label>+880 1874997427</label>
+            <i className='fa fa-envelope'></i>
+            <label>almamounhridoy113@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ's</label>
+            <label>Need Helps</label>
+            <span>BD</span>
+            <label htmlFor="">EN</label>
+            <span>BD</span>
+            <label htmlFor="">USD</label>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
