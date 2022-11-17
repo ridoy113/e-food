d5f071b2ed9d510a9b310cb5c4fc09689792b2e1
ridoy113 <almamounhridoy113@gmail.com>
2022-11-17 16:15:35 +0600

    project update'

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Head from '../header/Head';
 import Search from '../header/Search';
 import Navbar from '../header/Navbar';
+import './Header.css';
+
+
+
 
 const Header = () => {
     return (
